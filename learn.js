b410b31843cc5d168b9c0e24bb645e8c61ba8448
Salahuddin Rana <rana7cse@gmail.com>
2015-09-19 23:19:17 +0600

    learn_js init

---
+++ learn.js
@@ -0,0 +1,27 @@
+///---------------- Utility Functions are Here ------------
+function h1(heading){
+    console.log("//--------------------------//");
+    console.log("//-------->  "+heading);
+    console.log("//--------------------------//");
+}
+
+function h2(heading){
+    console.log("--------->  "+heading+"   <---------");
+}
+
+function q(question){
+    console.log("-- " + question + " ?");
+}
+///---------------- Utility Functions are Here ------------
+
+///--------- Learning Javascript Alert -------------
+h1('JavaScript Basic');
+h2('This is an Alert');
+alert('I am a Alert');
+
+//---------- Lets Learn variable ---------------
+h2('Learn Variable  ');
+q('What is variable');
+console.log("A variable is a reference name of memory space where we store the data as the value as the variable. \nHere is a example of javascript variable :");
+console.log("// var variableName ="+
+    "'Hello World'");

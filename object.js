9a9b78a394c093dcb37262d7d8ff399536353206
Salahuddin Rana <rana7cse@gmail.com>
2015-10-17 02:26:40 +0600

    call and apply and bind

--- object.js
+++ object.js
@@ -11,7 +11,7 @@
     console.log(myObj.propertyIsEnumerable('name' && 'set_name'));
     console.log(myObj.toString());
     console.log([1,2].toString());
-    
+
     // exact datatype found;
     console.log(Object.prototype.toString.call({}));
     console.log(Object.prototype.toString.call([]));
@@ -23,4 +23,48 @@
     console.log(Object.prototype.toString.call(undefined));
 
     // Basic Function;
+
+    function add(a,b){
+        return a,b;
+    }
+
+    var sub = function(a,b){
+        return a-b;
+    };
+
+    console.log(add.length);
+    console.log(sub.length);
+
+    // CALL AND APPLY BIND
+
+    var Voter = {
+        name : "Salahuddin Rana",
+        age : 24
+    };
+
+    function success(ID){
+        console.log("Hi "+this.name+" Your Age is "+this.age+" Successfully You get the ID: "+ID);
+    }
+
+    success.call(Voter,'FUCKFUCK','tik');
+
+    function setApply(func,object,args){
+        func.apply(object,args);
+    }
+
+    setApply(success,Voter,['pokot'])
+
+    function addItem(price,charge){
+        if(!this.total){
+            this.total = 0;
+        }
+        this.total += price += charge || 0;
+        return this.name +"'s purshed $"+this.total;
+    }
+
+    var buyItem = addItem.bind(Voter,1);
+
+    console.log(buyItem(100));
+    console.log(buyItem(100));
+    
 })();
